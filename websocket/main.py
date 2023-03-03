c4e78f91be923b6b06d3995ec1bb4fe5e6128ea5
hunkicho <jgchk4814@gmail.com>
2023-03-04 00:25:18 +0900

    [TEST] 게임종료 후 게임모드 정보 유지
    게임 종료 후 게임모드 정보 유지.

--- websocket/main.py
+++ websocket/main.py
@@ -486,7 +486,7 @@ class Notifier:
             self.survival_timer_task[room_name].cancel()
         self.room_info[room_name] = {}
         self.room_info[room_name]["is_start"] = 0
-        self.room_info[room_name]["game_mode"] = 0
+        #self.room_info[room_name]["game_mode"] = 0
         self.recent_turn_user[room_name] = {}
         self.turn_timer_task[room_name] = {}
         self.limit_timer_task[room_name] = {}
